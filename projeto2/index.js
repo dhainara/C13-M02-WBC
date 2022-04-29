794cce1b626ae52dab05007e4c4d1a92c06771bb
dhainara <daynaralinnyker@gmail.com>
2022-04-28 23:11:31 -0300

    last commit

--- projeto2/index.js
+++ projeto2/index.js
@@ -8,36 +8,25 @@ console.log(__dirname)
 
 const app = express() //instanciando o express dentro da const app
 
+app.use(express.urlencoded({extended: true}))
+app.use(express.json()) 
+
 app.set("view engine", "ejs") //faz com que o express reconehça o ejs como html
 app.use(express.static(path.join(__dirname, "public")))
 
 const port = 3001
 
 app.listen(port, () => { //listen é uma funcao para criar um servidor
-    console.log(`Running port ${port}`)
+    console.log(`Servidor rodando em https://localhost:${port}`)
 })
 
-const pokemon = [
+const dadosPokemon = [
     {
         id: 1,
-        numero: "653",
-        nome: "Fennekin",
-        tipo: "Fogo",
-        imagem: 'https://assets.pokemon.com/assets/cms2/img/pokemondex/full/653.png',
-        descricao: "Dhainara",
-        altura: "0,40m",
-        peso: "9,4kg",
-        categoria: "Fox",
-        habilidade: "Blaze",
-        deshab: "The Pokémon boosts its most proficient stat each time it knocks out a Pokémon."
-
-    },
-    {
-        id: 2,
         numero: "698",
         nome: "Amaura",
         tipo: "Pedra, Gelo",
-        imagem: 'https://assets.pokemon.com/assets/cms2/img/pokemondex/full/698.png',
+        imagem: './img/amaura.png',
         descricao: "Amaura é um pokémon tipo Pedra e Gelo. Ele é um pokemon ressucitado através de um Sail Fossil.",
         altura: "1.30m",
         peso: "25.2 kg",
@@ -46,11 +35,11 @@ const pokemon = [
         deshab: "The Pokémon boosts its most proficient stat each time it knocks out a Pokémon.",
     },
     {
-        id: 3,
+        id: 2,
         numero: "671",
         nome: "Florges",
         tipo: "Fada",
-        imagem: 'https://assets.pokemon.com/assets/cms2/img/pokemondex/full/671.png',
+        imagem: './img/florges.png',
         descricao: "Ele controla as flores que cresce. As nevascas de pétalas que Florges desencadeia são impressionantes em sua beleza e poder. ",
         altura: "1.10 m",
         peso: "10.0 kg",
@@ -59,11 +48,11 @@ const pokemon = [
         deshab: "The Pokémon boosts its most proficient stat each time it knocks out a Pokémon."
     },
     {
-        id: 4,
+        id: 3,
         numero: "803",
         nome: "Poipole",
         tipo: "Poison",
-        imagem: 'https://assets.pokemon.com/assets/cms2/img/pokemondex/full/803.png',
+        imagem: './img/poipole.png',
         descricao: "Este Ultra Beast é bastante apreciado para ser escolhido como o primeiro parceiro em seu próprio mundo.",
         altura: "0.60 m",
         peso: "1.80 kg",
@@ -72,11 +61,11 @@ const pokemon = [
         deshab: "The Pokémon boosts its most proficient stat each time it knocks out a Pokémon."
     },
     {
-        id: 5,
+        id: 4,
         numero: "417",
         nome: "Pachirisu",
         tipo: "Elétrico",
-        imagem: 'https://assets.pokemon.com/assets/cms2/img/pokemondex/full/417.png',
+        imagem: './img/pachirisu.png',
         descricao: "Ele faz bolas de pelo que estalam com eletricidade estática e os armazena com bagas em buracos de árvores.",
         altura: "0.40 m",
         peso: "3.90 kg",
@@ -85,11 +74,11 @@ const pokemon = [
         deshab: ["Enables a sure getaway from wild Pokémon.", "The Pokémon may pick up the item an opposing Pokémon used during a battle. It may pick up items outside of battle, too."]
     },
     {
-        id: 6,
+        id: 5,
         numero: "730",
         nome: "Primarina",
         tipo: "Agua, Fada",
-        imagem: 'https://assets.pokemon.com/assets/cms2/img/pokemondex/full/730.png',
+        imagem: './img/primarina.png',
         descricao: "Para Primaria, toda batalha é um estágio. Ele derruba sua presa com belos cantos e danças.",
         altura: "1.80 m",
         peso: "44.0 kg",
@@ -102,21 +91,21 @@ const pokemon = [
 
 app.get('/', (req, res) => { //get é um metodop do https/html que serve para trazer uma pagina
     res.render('index.ejs', {
-        pokemon
+        dadosPokemon
     })
 })
 
 app.get('/detalhes/:id/', (req, res) => {
-    let poke = []
-    pokemon.filter((element) => {
-        if (element.id == req.params.id) {
-            poke.push(element)
+    let pokemon = []
+    dadosPokemon.filter((element) => {
+        if (element.id === +req.params.id) {
+            pokemon=element
         }
     })
 
-    console.log(pokemon)
-    res.render('../views/detalhes', {
-        poke
+
+    res.render('detalhes.ejs', {
+        pokemon
     })
 })
 
@@ -125,15 +114,13 @@ app.get('/cadastro', (req, res) => {
     })
 })
 
+let i = dadosPokemon.length + 1
+
 app.post('/cadastro', (req, res) => {
-
-    let id = pokemon[pokemon.length-1]. id + 1
-
-    const { numero, nome, tipo, imagem, descricao, altura, peso, categoria, habilidade } = req.body
-
-    pokemon.push({ id: id, numero, nome, tipo, imagem, descricao, altura, peso, categoria, habilidade })
-
-    console.log(pokemon)
+    const { numero, nome, tipo, imagem, descricao, altura, peso, categoria, habilidades } = req.body
+    
+    dadosPokemon.push({ id: i, numero, nome, tipo, imagem, descricao, altura, peso, categoria, habilidades })
 
     res.redirect('/')
+
 })
